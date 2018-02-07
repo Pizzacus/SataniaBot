03c7572b0c22d8eb4bc2fd5ecff1ca7c8c4bb823
Pizzacus <pizzacus@gmail.com>
2018-02-07 09:24:34 +0100

    Satania will now list the channels she can be used in when used in a channel she can't speak in

--- src/inhibitors/perm-check.js
+++ src/inhibitors/perm-check.js
@@ -7,7 +7,16 @@ function exec(message) {
 		const perms = message.channel.permissionsFor(message.client.user);
 
 		if (!perms.has('SEND_MESSAGES')) {
-			return message.author.send('You cannot use this bot in this channel.')
+			return message.author.send({
+				embed: {
+					title: 'I cannot speak in this channel ;-;',
+					description: 'However, you can use me in the following channels:\n' +
+						message.guild.channels
+							.filter(channel => channel.type === 'text' && channel.permissionsFor(message.client.user).has('SEND_MESSAGES'))
+							.map(channel => ` • ${channel}`)
+							.join('\n')
+				}
+			})
 				.then(() => Promise.reject());
 		}
 	}
