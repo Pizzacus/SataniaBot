c6fdceccad7482cc6597bf0042bbcbdd3ac68c82
Pizzacus <pizzacus@gmail.com>
2018-03-16 00:54:38 +0100

    Prevent listing channels the user can't see or speak in Also show a special message if Satania can't speak anywhere

--- src/inhibitors/perm-check.js
+++ src/inhibitors/perm-check.js
@@ -7,16 +7,37 @@ function exec(message) {
 		const perms = message.channel.permissionsFor(message.client.user);
 
 		if (!perms.has('SEND_MESSAGES')) {
-			return message.author.send({
-				embed: {
-					title: 'I cannot speak in this channel ;-;',
-					description: 'However, you can use me in the following channels:\n' +
-						message.guild.channels
-							.filter(channel => channel.type === 'text' && channel.permissionsFor(message.client.user).has('SEND_MESSAGES'))
-							.map(channel => ` • ${channel}`)
-							.join('\n')
-				}
-			}).then(() => Promise.reject());
+			const usuableChannels = message.guild.channels.filter(
+				channel =>
+					channel.type === 'text' &&
+					channel
+						.permissionsFor(message.client.user)
+						.has('VIEW_CHANNEL', 'SEND_MESSAGES') &&
+					channel
+						.permissionsFor(message.author)
+						.has('VIEW_CHANNEL', 'SEND_MESSAGES')
+			);
+
+			let description;
+
+			if (usuableChannels.size > 0) {
+				description =
+					'However, you can use me in the following channels:\n' +
+					usuableChannels.map(channel => ` • ${channel}`).join('\n');
+			} else {
+				description =
+					'And it seems like you can\'t use me in any channel ' +
+					'on this server, maybe you should tell a mod about that >_<';
+			}
+
+			return message.author
+				.send({
+					embed: {
+						title: 'I cannot speak in this channel ;-;',
+						description
+					}
+				})
+				.then(() => Promise.reject());
 		}
 	}
 }
