3aa22c3e7012825716071ec0e5af5538bcd0571b
Pizzacus <pizzacus@gmail.com>
2018-04-11 20:37:59 +0200

    Add s!echo command

---
+++ src/commands/echo.js
@@ -0,0 +1,39 @@
+const {Command} = require('discord-akairo');
+
+const nick = requireUtil('nick');
+
+const options = {
+	aliases: ['echo', 'say', 'repeat'],
+	args: [
+		{
+			id: 'text',
+			match: 'content'
+		}
+	],
+	description: 'Repeat whatever you add after the command'
+};
+
+function exec(message, {text}) {
+	return message.channel.send(
+		text.replace(/<@[!&]?(\d+)>/g, (match, id) => {
+			const user = message.client.users.get(id);
+			const role = message.guild ?
+				message.guild.roles.get(id) :
+				null;
+
+			let mention;
+
+			if (user) {
+				mention = '@' + nick(user, message.channel);
+			} else if (role) {
+				mention = '@' + role.name;
+			} else {
+				mention = match;
+			}
+
+			return '**' + mention.replace(/<(@[!&]?\d+)>/g, '<\u2060$1>') + '**';
+		})
+	);
+}
+
+module.exports = new Command('echo', exec, options);
