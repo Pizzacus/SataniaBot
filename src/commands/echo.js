7c25aa935608c5bc67882fb5ebc537d95945bc39
Pizzacus <pizzacus@gmail.com>
2018-08-28 12:40:37 +0200

    Prevent people from abusing s!echo

--- src/commands/echo.js
+++ src/commands/echo.js
@@ -13,7 +13,24 @@ const options = {
 	description: 'Repeat whatever you add after the command'
 };
 
+const echoedMessages = new Map();
+
+let eventHandler;
+
 function exec(message, {text}) {
+	if (!eventHandler) {
+		eventHandler = message.client.on('messageDelete', message => {
+			const echoed = echoedMessages.get(message.id);
+
+			if (echoed) {
+				echoed.edit(
+					`**${nick(message.author, message.channel)}**, to prevent abuse, you must wait at least 15 seconds before removing your command`
+				);
+
+				echoed.delete(7500);
+			}
+		});
+	}
 	if (typeof text !== 'string' || text.length === 0) {
 		return message.reply('please specify something to say');
 	}
@@ -37,7 +54,12 @@ function exec(message, {text}) {
 
 			return '**' + mention.replace(/<(@[!&]?\d+)>/g, '<\u2060$1>') + '**';
 		})
-	);
+	).then(echoMessage => {
+		echoedMessages.set(message.id, echoMessage);
+		setTimeout(() => echoedMessages.delete(message.id), 15000);
+
+		return echoedMessages;
+	});
 }
 
 module.exports = new Command('echo', exec, options);
