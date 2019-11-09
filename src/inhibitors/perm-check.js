07eadc7be33180b923af966349daebf2e7920836
Pizzacus <mail@pizzacus.com>
2019-11-10 00:21:39 +0100

    Do not DM people about perms for special commands
    Using a special command like "praise satania" or "good bot"
    in a channel where satania has no perms would previously
    cause Satania to DM the user.
    
    Now this is no longer the case because this may not be expected.

--- src/inhibitors/perm-check.js
+++ src/inhibitors/perm-check.js
@@ -2,42 +2,45 @@ const {Inhibitor} = require('discord-akairo');
 
 const options = {};
 
-function exec(message) {
+async function exec(message, command) {
 	if (message.channel.type === 'text') {
 		const perms = message.channel.permissionsFor(message.client.user);
 
 		if (!perms.has('SEND_MESSAGES')) {
-			const usuableChannels = message.guild.channels.filter(
-				channel =>
-					channel.type === 'text' &&
-					channel
-						.permissionsFor(message.client.user)
-						.has('VIEW_CHANNEL', 'SEND_MESSAGES') &&
-					channel
-						.permissionsFor(message.author)
-						.has('VIEW_CHANNEL', 'SEND_MESSAGES')
-			);
+			if (!(await command.trigger())) {
+				const usuableChannels = message.guild.channels.filter(
+					channel =>
+						channel.type === 'text' &&
+						channel
+							.permissionsFor(message.client.user)
+							.has('VIEW_CHANNEL', 'SEND_MESSAGES') &&
+						channel
+							.permissionsFor(message.author)
+							.has('VIEW_CHANNEL', 'SEND_MESSAGES')
+				);
 
-			let description;
+				let description;
 
-			if (usuableChannels.size > 0) {
-				description =
-					'However, you can use me in the following channels:\n' +
-					usuableChannels.map(channel => ` • ${channel}`).join('\n');
-			} else {
-				description =
-					'And it seems like you can\'t use me in any channel ' +
-					'on this server, maybe you should tell a mod about that >_<';
+				if (usuableChannels.size > 0) {
+					description =
+						'However, you can use me in the following channels:\n' +
+						usuableChannels.map(channel => ` • ${channel}`).join('\n');
+				} else {
+					description =
+						'And it seems like you can\'t use me in any channel ' +
+						'on this server, maybe you should tell a mod about that >_<';
+				}
+
+				await message.author
+					.send({
+						embed: {
+							title: 'I cannot speak in this channel ;-;',
+							description
+						}
+					});
 			}
 
-			return message.author
-				.send({
-					embed: {
-						title: 'I cannot speak in this channel ;-;',
-						description
-					}
-				})
-				.then(() => Promise.reject());
+			return Promise.reject();
 		}
 	}
 }
