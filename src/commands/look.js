091d7ad5b20c7bb36c6a68c18d7e7a1abf7a798c
Pizzacus <mail@pizzacus.com>
2019-11-19 19:26:55 +0100

    Only convert images that must be converted

--- src/commands/look.js
+++ src/commands/look.js
@@ -5,18 +5,46 @@ const sharp = require('sharp');
 const {Inflectors} = require('en-inflectors');
 const indefinite = require('indefinite');
 const lexi = require('en-lexicon');
+const fileType = require('file-type');
 
 const {optimalDensity} = requireUtil('svg-utils');
 const config = requireUtil('config');
 const relevantLink = requireUtil('relevant-link');
 const fetchImage = requireUtil('fetch-image');
+const formats = requireUtil('get-supported-formats')();
 
 // NLP override
 const ARE_NOUNS = new Set(['costume', 'constructor', 'cityscape', 'blur', 'wear', 'outdoors', 'kimono', 'closeup', 'accessory']);
 const NOT_NOUNS = new Set(['fun', 'kind', 'disjunct', 'ball-shaped', 'vintage']);
 const SKIP_WORDS = new Set(['one', 'people', 'no person', 'graphic', 'vector']);
 const ARE_COUNTABLE = new Set(['woman']);
-const NOT_COUNTABLE = new Set(['horror', 'springtime', 'summer', 'fall', 'winter', 'baking', 'affection', 'togetherness', 'romance', 'ivory', 'fashion', 'art', 'marijuana', 'healthcare', 'cannabis', 'plastic', 'geography', 'simplicity', 'creativity', 'swimming', 'glamour', 'vandalism', 'fright', 'curiosity', 'patriotism']);
+const NOT_COUNTABLE = new Set([
+	'horror',
+	'springtime',
+	'summer',
+	'fall',
+	'winter',
+	'baking',
+	'affection',
+	'togetherness',
+	'romance',
+	'ivory',
+	'fashion',
+	'art',
+	'marijuana',
+	'healthcare',
+	'cannabis',
+	'plastic',
+	'geography',
+	'simplicity',
+	'creativity',
+	'swimming',
+	'glamour',
+	'vandalism',
+	'fright',
+	'curiosity',
+	'patriotism'
+]);
 const NOUN_LIKE = ['NN', 'NNS', 'NNP', 'NNPS', 'VBG'];
 const ADJECTIVE_LIKE = ['JJ', 'CD'];
 
@@ -53,19 +81,39 @@ async function exec(message, args) {
 		return;
 	}
 
+	console.log(`[${new Date().toLocaleTimeString()}] Look request from: ${message.author.tag} (${message.author.id})`);
+
 	await message.channel.startTyping();
 
-	const image = await fetchImage(link.url);
-	const converted = await sharp(image, {
-		density: optimalDensity(image, 800, 800)
-	})
-		.toFormat('png')
-		.toBuffer();
+	let image = await fetchImage(link.url, {
+		imageMime: [
+			...formats,
+			'image/bmp'
+		]
+	});
+	const type = fileType(image);
+
+	// The Clarifai docs say they support WEBP but this desn't appear to be the case
+	if (!type || !['jpg', 'png', 'tif', 'bmp'].includes(type.ext)) {
+		const img = await sharp(image, {
+			density: optimalDensity(image, 800, 800)
+		});
+
+		const stats = await img.stats();
+
+		if (stats.isOpaque) {
+			img.toFormat('jpg');
+		} else {
+			img.toFormat('png');
+		}
+
+		image = await img.toBuffer();
+	}
 
 	const res = await clarifai.models.predict(
 		Clarifai.GENERAL_MODEL,
 		{
-			base64: converted.toString('base64')
+			base64: image.toString('base64')
 		}
 	);
 
@@ -98,8 +146,6 @@ async function exec(message, args) {
 					.split(/\|/g)
 					.find(purpose => [...NOUN_LIKE, ...ADJECTIVE_LIKE].includes(purpose));
 			}
-
-			console.log(purpose, lexi.lexicon[word]);
 
 			if (NOUN_LIKE.includes(purpose)) {
 				// It's a noun
@@ -143,7 +189,7 @@ async function exec(message, args) {
 	embed.setDescription(`On this image, I see ${list(nouns.slice(0, 10))}.\n\nIt looks ${list(adjectives.slice(0, 10))}.`);
 	embed.attachFiles([{
 		name: 'image.png',
-		attachment: converted
+		attachment: image
 	}]);
 	embed.setImage('attachment://image.png');
 
