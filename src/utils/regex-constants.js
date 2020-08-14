b9df78fc2e208a0187e4bd202ca4e023ec3eac69
Daniel <luffydafloffi@gmail.com>
2020-08-15 01:29:27 +0300

    Added Korean and Chinese variants of Satania's name

--- src/utils/regex-constants.js
+++ src/utils/regex-constants.js
@@ -1,7 +1,7 @@
 const reg = require('./reg');
 
 const kurumizawa = /(kurumizawa|胡桃沢)?\s*/;
-const satania = /(satan(i|y|ichi)a|Сатании|サタニキア|サターニャ)\s*-?/;
+const satania = /(satan(i|y|ichi)a|Сатании|サタニキア|サターニャ|사타냐|萨塔妮娅)\s*-?/;
 const mcdowell = /(mcdowellu?|マクドウェル)?\s*/;
 const honorifics = /(sama|san|chan|senpai|さま|様|さん|ちゃん|せんぱい|先輩)?\s*/;
 const extra = /[!！¡﹗︕‼¿？⁉﹖︖⁈⁇?~˜～〜]?/;
