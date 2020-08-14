fe71d4b502634bfa35be32a30b03e10086319d07
Daniel <luffydafloffi@gmail.com>
2020-08-15 01:15:31 +0300

    Regex group for the punctuation

--- src/utils/regex-constants.js
+++ src/utils/regex-constants.js
@@ -4,7 +4,7 @@ const kurumizawa = /(kurumizawa|胡桃沢)?\s*/;
 const satania = /(satan(i|y|ichi)a|Сатании|サタニキア|サターニャ)\s*-?/;
 const mcdowell = /(mcdowellu?|マクドウェル)?\s*/;
 const honorifics = /(sama|san|chan|senpai|さま|様|さん|ちゃん|せんぱい|先輩)?\s*/;
-const extra = /(!+|！+|¡+|﹗+|︕+|‼+|¿+|？+|⁉+|﹖+|︖+|⁈+|⁇+|\?+|~+|˜+|～+|〜+)?/;
+const extra = /[!！¡﹗︕‼¿？⁉﹖︖⁈⁇?~˜～〜]?/;
 const sataniaName = reg`${kurumizawa}${satania}${mcdowell}${honorifics}`;
 
 module.exports = {
