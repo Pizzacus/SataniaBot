6c49f3d175e0f3d053df1ba6168ce5bcdb398201
Pizzacus™ <mail@pizzacus.com>
2020-08-15 08:54:11 +0200

    Allow any number of punctuation

--- src/utils/regex-constants.js
+++ src/utils/regex-constants.js
@@ -4,7 +4,7 @@ const kurumizawa = /(kurumizawa|胡桃沢)?\s*/;
 const satania = /(satan(i|y|ichi)a|Сатании|サタニキア|サターニャ|사타냐|萨塔妮娅)\s*-?/;
 const mcdowell = /(mcdowellu?|マクドウェル)?\s*/;
 const honorifics = /(sama|san|chan|senpai|さま|様|さん|ちゃん|せんぱい|先輩)?\s*/;
-const extra = /[!！¡﹗︕‼¿？⁉﹖︖⁈⁇?~˜～〜]?/;
+const extra = /[!！¡﹗︕‼¿？⁉﹖︖⁈⁇?~˜～〜]*/;
 const sataniaName = reg`${kurumizawa}${satania}${mcdowell}${honorifics}`;
 
 module.exports = {
